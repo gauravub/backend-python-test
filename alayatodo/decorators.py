ac7df2a2faa65484eee649ef958a23494328690f
Gaurav Redkar <redkar_gaurav@yahoo.co.in>
2018-07-27 10:30:05 -0400

    FIX-3: Added decorator for checking login status as well added flash messages to notify user of his activity

---
+++ alayatodo/decorators.py
@@ -0,0 +1,18 @@
+from functools import wraps
+from flask import (
+    redirect,
+    session,
+    url_for,
+    flash
+    )
+
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first')
+            return redirect(url_for('login'))
+    return wrap
--- alayatodo/views.py
+++ alayatodo/views.py
@@ -5,9 +5,11 @@ from flask import (
     render_template,
     request,
     session,
-    jsonify
+    jsonify,
+    flash
     )
 from alayatodo.models import User, Todo
+from decorators import login_required
 
 
 @app.route('/')
@@ -36,13 +38,16 @@ def login_post():
     if user:
         session['user'] = user.get_dict()
         session['logged_in'] = True
+        flash('Welcome {}'.format(user.username))
         return redirect('/todo')
-
-    return redirect('/login')
+    else:
+        flash('Incorrect login credentials. Kindly try again.')
+        return redirect('/login')
 
 
 @app.route('/logout')
 def logout():
+    flash('You were logged out.')
     session.pop('logged_in', None)
     session.pop('user', None)
     return redirect('/')
@@ -58,6 +63,7 @@ def todo(id):
     return render_template('todo.html', todo=todo_item)
 
 @app.route('/todo/<id>/json', methods=['GET'])
+@login_required
 def get_json(id):
     todo_item = (
         g.db.session.query(Todo)
@@ -71,9 +77,8 @@ def get_json(id):
 # urls that match without trailing slash will trigger a redirect to the same
 # URL with the missing slash appended
 @app.route('/todo/', methods=['GET'])
+@login_required
 def todos_get():
-    if not session.get('logged_in'):
-        return redirect('/login')
     page = int(request.args.get('page', 1))
     todo_results = (
         g.db.session.query(Todo)
@@ -84,32 +89,31 @@ def todos_get():
 
 
 @app.route('/todo/', methods=['POST'])
+@login_required
 def todos_post():
-    if not session.get('logged_in'):
-        return redirect('/login')
     user_id = session['user']['id']
     description = request.form.get('description')
     new_todo = Todo(user_id=user_id, description=description, complete=0)
     g.db.session.add(new_todo)
     g.db.session.commit()
+    flash('New Todo item was added with id {}.'.format(new_todo.id))
     return redirect('/todo')
 
 
 @app.route('/todo/<int:id>', methods=['POST'])
+@login_required
 def todo_delete(id):
-    if not session.get('logged_in'):
-        return redirect('/login')
+
     item = Todo.query.filter_by(user_id=session['user']['id'],
                                  id=id).one_or_none()
     if item:
+        flash('Todo item with id {} deleted.'.format(item.id))
         g.db.session.delete(item)
         g.db.session.commit()
     return redirect('/todo')
 
 @app.route('/todo/<id>/toggle_status', methods=['POST'])
 def toggle_status(id):
-    if not session.get('logged_in'):
-        return redirect('/login')
     item = Todo.query.filter_by(user_id=session['user']['id'],
                                  id=id).one_or_none()
     if item:
